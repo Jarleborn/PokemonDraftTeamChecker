677594623e1969df44c9e63b46f2038cbf70a5f0
Jarleborn <hj222hi@student.lnu.se>
2017-10-29 02:26:33 +0200

    added footer with contact details

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,34 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import PokeFrame from './components/PokeFrame'
-
+import {Footer} from 'react-materialize'
 class App extends Component {
 
 
   render() {
     return (
       <div className= 'App '>
-        <header className= 'App-header '>
-          <img src={logo} className= 'App-logo ' alt= 'logo ' />
-          <h1 className='App-title'>Pokemon Draft Team Checker</h1>
-          <p> Update the page to try another team </p>
-        </header>
-        <PokeFrame />
+      <header className= 'App-header '>
+      <img src={logo} className= 'App-logo ' alt= 'logo ' />
+      <h1 className='App-title'>Pokemon Draft Team Checker</h1>
+      <p> Update the page to try another team </p>
+      </header>
+      <PokeFrame />
+      <Footer copyrights="&copy 2017 Copyright Text"
+      moreLinks={
+        <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+      }
+      links={
+        <ul>
+        <li><a className="grey-text text-lighten-3" target="_blank" href="https://github.com/Jarleborn/PokemonDraftTeamChecker">GitHub</a></li>
+        <li><a className="grey-text text-lighten-3" target="_blank" href="https://discord.gg/Pq3bUHr">Discord</a></li>
+        </ul>
+      }
+      className='example'
+      >
+      <p className="white-text">Here's the contact info</p>
+
+      </Footer>
       </div>
     )
   }
